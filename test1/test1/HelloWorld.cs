b564d26f442b157aa5981b630c6125e0db1d371a
sangjin <tkdwls9277@naver.com>
2020-06-29 20:05:41 +0900

    C# study

---
+++ test1/test1/HelloWorld.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace test1
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat { Age = 10, Name = "Fluffy" };
+
+            var pet = new { Age = 10, Name = "Fluffy" };
+
+            List<Cat> cats = new List<Cat>
+            {
+                new Cat() {Name="Tester1", Age=9},
+                new Cat() {Name="Tester2", Age=10}
+            };
+
+            WriteLine(cat.Name);
+
+            foreach (Cat c in cats) {
+                WriteLine(c.Name);
+            }
+        }
+    }
+}
