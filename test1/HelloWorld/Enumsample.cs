39b9b8bd3a6bbd3fb58359821249173e915cb297
tkdwls9277 <tkdwls9277@naver.com>
2020-07-02 19:22:28 +0900

    c# study

---
+++ test1/HelloWorld/Enumsample.cs
@@ -0,0 +1,7 @@
+using System;
+using static System.Console;
+
+class Enumsample
+{
+    enum Animal { mouse, cat, bird, dog=100, koala, pig=200, lion};
+}
--- test1/HelloWorld/Program.cs
+++ test1/HelloWorld/Program.cs
@@ -10,23 +10,27 @@ namespace HelloWorld
 {
     class Program
     {
+        enum Animal { mouse, cat, bird, dog=100, koala, pig=200, lion};
         static void Main(string[] args)
         {
             MyClass cls = new MyClass();
+            Enumsample enumsample = new Enumsample();
 
-            //인덱서 set 사용
-            cls[0] = 0;
-            cls[1] = 1;
-            cls[2] = 2;
-            cls[3] = 3;
+            Animal animal;
+            animal = Animal.dog;
 
-            //인덱서 get 사용
-            for(int i=0;i<=3;i++){
-                WriteLine(cls[i]);
-            }
+            WriteLine(Animal.cat);
+            WriteLine((int)Animal.dog);
+            WriteLine((int)Animal.lion);
+            WriteLine(animal);
 
-            WriteLine(cls);
             ReadKey(true);
         }
     }
+
+
 }
+    void Divided(int a, int b, out int quotient, out int remainder){
+        quotient = a/b;
+        remainder = a%b;
+    }
