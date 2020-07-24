3bdd32757dee34e4d928f3f7c29faf9d398e86d1
sangjin <tkdwls9277@naver.com>
2020-07-24 10:10:34 +0900

    11

---
+++ test1/11test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _11test
+{
+    class MyList<T>
+    {
+        private T[] array;
+
+        public MyList() {
+            array = new T[3];
+        }
+        
+        public T this[int index]{
+            get{
+                return array[index];
+            }
+            set{
+                if(index >= array.Length)
+                {
+                    Array.Resize<T>(ref array, index + 1);
+                    Console.WriteLine($"Array Resized : {array.Length}");
+                }
+                array[index]=value;
+            }
+        }
+
+        public int Length{
+            get{return array.Length;}
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!1");
+            Console.WriteLine("Hello World!2");
+            Console.WriteLine("Hello World!3");
+        }
+    }
+}
