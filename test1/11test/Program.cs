3bd0a17faf7fb6aa5928812bf3e1b15c402cb13c
sangjin <tkdwls9277@naver.com>
2020-07-27 17:34:41 +0900

    0727 c# study

--- test1/11test/Program.cs
+++ test1/11test/Program.cs
@@ -32,9 +32,33 @@ namespace _11test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!1");
-            Console.WriteLine("Hello World!2");
-            Console.WriteLine("Hello World!3");
+            MyList<string> str_list = new MyList<string>();
+
+            str_list[0] = "abc";
+            str_list[1] = "def";
+            str_list[2] = "ghi";
+            str_list[3] = "jkl";
+            str_list[4] = "mno";
+
+            for(int i =0;i<str_list.Length;i++){
+                Console.WriteLine(str_list[i]);
+            }
+
+            Console.WriteLine();
+
+            MyList<int> int_list = new MyList<int>();
+
+            int_list[0] = 0;
+            int_list[1] = 1;
+            int_list[2] = 2;
+            int_list[3] = 3;
+            int_list[4] = 4;
+
+            for(int i=0;i<int_list.Length;i++){
+                Console.WriteLine(int_list[i]);
+            }
+            Console.WriteLine();
+
         }
     }
 }
